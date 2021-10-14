4d9cb676b8d2b18fedcbb84bea6258988dd95d11
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2021-10-14 23:49:28 +0300

    trying to make it work correctly (still fixing timestamps)

--- homework.py
+++ homework.py
@@ -85,7 +85,7 @@ def send_message(message):
 
 def main():
     """Executable code."""
-    current_timestamp = int(time.time()) - 3 * 60 * 60
+    current_timestamp = int(time.time())
 
     while True:
         try:
@@ -95,8 +95,8 @@ def main():
                 homework = homeworks_list[0]
                 message = parse_homework_status(homework)
                 send_message(message)
-            time.sleep(5 * 60)
-            current_timestamp = int(time.time()) - 3 * 60 * 60
+            time.sleep(2)
+            current_timestamp = int(time.time())
 
         except Exception as e:
             logger.error(f'Бот упал с ошибкой: {e}', exc_info=True)
